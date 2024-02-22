332210dcc762d24eee64c43e49ca0ab18f367082
Aakarbharadwaj <104188303+Aakarbharadwaj@users.noreply.github.com>
2024-02-22 21:22:35 +0530

    food odering app

--- food/src/components/Card/Card.jsx
+++ food/src/components/Card/Card.jsx
@@ -1,14 +1,24 @@
 import React from 'react'
 import './card.css'
+
+
+const Items = {
+    url: 'https://www.tasteofhome.com/wp-content/uploads/2021/01/tasty-butter-chicken-curry-dish-from-indian-cuisine-1277362334.jpg',
+    title: "kadhai paneer",
+    rating: "4.8"
+}
+
 const Card = () => {
     return (
         <div className='card-box'>
             <div className='card'>
-                <img src={'https://www.tasteofhome.com/wp-content/uploads/2021/01/tasty-butter-chicken-curry-dish-from-indian-cuisine-1277362334.jpg'}></img>
+                <img src={Items.url}></img>
+                <h3 className='brand'>{Items.title}</h3>
                 <div className='title'>
                     <h3>Top India dish</h3>
-                    <h3 className='rating'>4.9</h3>
+                    <h3 className='rating'>{Items.rating}</h3>
                 </div>
+
             </div>
         </div>
     )
